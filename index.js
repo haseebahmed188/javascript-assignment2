627469b795e15f1d25d625efeec815a0489bff21
HASEEB AHMED <155947094+haseebahmed188@users.noreply.github.com>
2024-01-21 14:33:25 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+var length = 8;  
+var width = 9;   
+var area = length * width;
+console.log("The area of the rectangle is: " , area);
---
+++ index2.js
@@ -0,0 +1,4 @@
+var userName = prompt("What is your name?");
+if (userName !== null) {
+  alert("Hello, " + userName + "! Welcome to our website!");
+} 
